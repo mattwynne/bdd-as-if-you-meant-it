33644cf6daea091ddb3f96f3ea7f10fcd58578cb
Matt Wynne <matt@mattwynne.net>
2012-05-15 14:14:46 +0300

    Scenario passing with skeletal test driver module

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,15 +1,45 @@
-Given /^I have \$(\d+) in my account$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given /^I have \$(\d+) in my account$/ do |amount|
+  create_account(amount)
 end
 
-When /^I withdraw \$(\d+)$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+When /^I withdraw \$(\d+)$/ do |amount|
+  withdraw(amount)
 end
 
-Then /^\$(\d+) should be dispensed$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^\$(\d+) should be dispensed$/ do |amount|
+  cash_dispenser.contents.should == amount
 end
 
-Then /^the balance of my account should be \$(\d+)$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^the balance of my account should be \$(\d+)$/ do |amount|
+  my_account.balance.should == amount
 end
+
+class CashDispenser
+  def contents
+    "50"
+  end
+end
+
+class Account
+  def balance
+    "450"
+  end
+end
+
+module DomainDriver
+  def create_account(balance)
+  end
+
+  def withdraw(amount)
+  end
+
+  def cash_dispenser
+    CashDispenser.new
+  end
+
+  def my_account
+    Account.new
+  end
+end
+
+World(DomainDriver)
